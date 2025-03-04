b02a8bc8014f92f56a9afa7d4f4bc6973fa32341
Jeff Fry <jeff.fry@wwt.com>
2025-03-03 21:36:11 -0500

    Commited the updated config.py file. PLan is to move this to a .env file in the future.

--- config.py
+++ config.py
@@ -1,3 +1,27 @@
 # This file needs to be edit for your google API key
 
-google_api_key = "xxxxxxxxxxxxxxx-xxxxx-xxxxx-xxxxxxxxxxx"
+# Enter your Google API Key
+google_api_key = "xxxxxxxxxxxxx-xxx"
+
+# Enter your Mist Org
+mist_org = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+# Enter your Mist API Key
+mist_api_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# Remove the comment from the region that your org is in.
+# Below we are enabled for Global 03
+#mist_api_url = 'api.mist.com' #      Global 01
+#mist_api_url = 'api.gc1.mist.com' #  Global 02
+mist_api_url = "api.ac2.mist.com" #  Global 03
+#mist_api_url = 'api.gc2.mist.com' #  Global 04
+#mist.api.url = 'api.gc4.mist.com' #  Global 05
+#mist_api_url = 'api.eu.mist.com' #   EMEA 01
+#mist_api_url = 'api.gc3.mist.com' #  EMEA 02
+#mist_api_url = 'api.aci6.mist.com' # EMEA 03
+#mist_api_url = 'api.ac5.mist.com' #  APAC 01
+
+# If you get "InsecureRequestWarning: Unverified HTTPS request is being made to host" you can enable this and will remove those errors
+# These are caused by URL filtering applications ( Cisco Umbrella, etc )
+# To enable, just remove the # on the line below
+#ssldisable = 'yes'
